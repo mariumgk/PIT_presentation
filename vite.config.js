e005d7e31a218034c642518f8d88d3506ff94f54
mariumgk <mariumgk@github.com>
2026-05-11 21:01:04 +0500

    fix: update base path for PIT_presentation GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/PIT_S-P/',
+  base: '/PIT_presentation/',
 })
